758ba33f9698d1ceb0522e5a05fdd0bc4b425e0c
diegofreitas <diegofreitas@gmail.com>
2013-04-20 10:08:59 -0400

    adicionado interacao com usuario

--- src/br/fucapi/pizzaria/PizzaTestDrive.java
+++ src/br/fucapi/pizzaria/PizzaTestDrive.java
@@ -3,6 +3,8 @@ package br.fucapi.pizzaria;
 import java.util.Scanner;
 
 public class PizzaTestDrive {
+	
+	private static  PizzaStore store = new PizzaStore();
 
 	/**
 	 * @param args
@@ -13,11 +15,9 @@ public class PizzaTestDrive {
 		System.out.println("Pizzaria Massa Suja");
 		while(true){
 			System.out.println("Qual é a pizza:");
-			System.out.println("Qual é a pizza:");
-			System.out.println("Qual é a pizza:");
 			String nextLine = scan.nextLine();
 			
-			
+			store.orderPizza(nextLine);
 			
 			System.out.print("deseja continuar: ");
 			nextLine = scan.nextLine();
